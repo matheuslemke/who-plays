7420af18f6afd1a0751bcb7951f49283dc2e4c8a
Anderson Lemke <amatheuslemke@hotmail.com>
2024-01-02 12:04:43 -0300

    feat: add date for match

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import { CSMatchesController } from "@/controllers/csMatchesController"
 import { Match } from "@/types/Match"
 import tw from "tailwind-styled-components"
 import { AddToCalendar } from "./components/AddToCalendar"
+import dayjs from "dayjs"
 
 const Game = tw.span`
   text-xs my-auto md:text-base
 `
 const TeamName = tw.div`
-  text-xs text-ellipsis text-center md:text-lg
+  text-xs text-ellipsis text-center self-center md:text-lg
 `
 
 const getMatches = async (): Promise<Match[]> =>
@@ -26,18 +27,17 @@ export default async function Home() {
               className="border w-full grid grid-cols-[1fr_repeat(3,_2fr)_1fr] gap-3 md:gap-4"
             >
               <Game>{match.game}</Game>
-              <div>
-                <TeamName>{match.home.name}</TeamName>
-              </div>
+              <TeamName>{match.home.name}</TeamName>
               <div className="flex flex-col items-center">
+                <span className="text-[8px] text-center md:text-xs">
+                  {dayjs(match.date).format("DD/MMM")}
+                </span>
                 <span className="text-4xl">{match.date.substring(16, 21)}</span>
                 <span className="text-[8px] text-center md:text-xs">
                   {match.league}
                 </span>
               </div>
-              <div>
-                <TeamName>{match.away.name}</TeamName>
-              </div>
+              <TeamName>{match.away.name}</TeamName>
               <div className="flex items-center mr-1">
                 <AddToCalendar match={match} />
               </div>
