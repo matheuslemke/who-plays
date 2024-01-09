3cd5769baae3c400bcfa289efd98923947e016ee
Anderson Lemke <amatheuslemke@hotmail.com>
2024-01-09 10:41:09 -0300

    feat: add plano matches

--- app/controllers/csMatchesController.ts
+++ app/controllers/csMatchesController.ts
@@ -5,14 +5,17 @@ const getMatches = async (): Promise<Match[]> => {
   const furiaId = 364396
   const mibrId = 366538
   const imperialId = 365850
+  const planoId = 381450
 
-  const [furiaMatches, mibrMatches, imperialMatches] = await Promise.all([
-    CSMatchesResource.getMatches(furiaId),
-    CSMatchesResource.getMatches(mibrId),
-    CSMatchesResource.getMatches(imperialId),
-  ])
+  const [furiaMatches, mibrMatches, imperialMatches, planoMatches] =
+    await Promise.all([
+      CSMatchesResource.getMatches(furiaId),
+      CSMatchesResource.getMatches(mibrId),
+      CSMatchesResource.getMatches(imperialId),
+      CSMatchesResource.getMatches(planoId),
+    ])
 
-  return [...furiaMatches, ...mibrMatches, ...imperialMatches]
+  return [...furiaMatches, ...mibrMatches, ...imperialMatches, ...planoMatches]
 }
 
 const CSMatchesController = {
--- app/resources/csMatchesResource.ts
+++ app/resources/csMatchesResource.ts
@@ -22,26 +22,31 @@ const mapper = (response: NextMatchesResponse): Match[] => {
 
 const getMatches = async (teamId: number): Promise<Match[]> => {
   try {
-    // const response = await fetch(
-    //   `${process.env.CSGO_URL}/team/${teamId}/matches/next/0`,
-    //   {
-    //     method: "GET",
-    //     headers: {
-    //       "X-RapidAPI-Key": process.env.CSGO_KEY || "",
-    //       "X-RapidAPI-Host": "esportapi1.p.rapidapi.com",
-    //     },
-    //     next: { revalidate: 3600 },
-    //   }
-    // )
-    // if (Number(response.headers.get("content-length")) > 0) {
-    const result: NextMatchesResponse = nextFazeResponseMock // await response.json()
-    return mapper(result)
-    // }
+    const response = await fetch(
+      `${process.env.CSGO_URL}/team/${teamId}/matches/next/0`,
+      {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": process.env.CSGO_KEY || "",
+          "X-RapidAPI-Host": "esportapi1.p.rapidapi.com",
+        },
+        next: { revalidate: 3600 },
+      }
+    )
+    if (Number(response.headers.get("content-length")) > 0) {
+      const result: NextMatchesResponse = await response.json()
+      return mapper(result)
+    }
   } catch (error) {
     console.error(error)
   }
   return []
 }
+
+// const getMatches = async (teamId: number): Promise<Match[]> => {
+//   const result: NextMatchesResponse = nextFazeResponseMock
+//   return mapper(result)
+// }
 
 const CSMatchesResource = { getMatches }
 
