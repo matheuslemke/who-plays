7181749983a025feae743c0869cf5704e25aef81
Anderson Lemke <amatheuslemke@hotmail.com>
2024-01-15 09:57:46 -0300

    feat: add separator for dates

--- app/page.tsx
+++ app/page.tsx
@@ -35,28 +35,34 @@ export default async function Home() {
     <main>
       <div className="w-screen">
         <div className="flex flex-col items-center">
-          {matches.map((match) => (
-            <div
-              key={match.id}
-              className="border w-full grid grid-cols-[1fr_repeat(3,_2fr)_1fr] gap-3 md:gap-4"
-            >
-              <GameIcon game={match.game} />
-              <Team team={match.home} />
-              <div className="flex flex-col items-center">
-                <span className="text-[8px] text-center md:text-xs">
-                  {dayjs(match.date).tz("America/Sao_Paulo").format("DD/MMM")}
-                </span>
-                <span className="text-4xl">
-                  {dayjs(match.date).tz("America/Sao_Paulo").format("HH:mm")}
-                </span>
-                <span className="text-[8px] text-center md:text-xs">
-                  {match.league}
-                </span>
+          {matches.map((match, index) => (
+            <div key={match.id} className="w-full">
+              <div className="border w-full grid grid-cols-[1fr_repeat(3,_2fr)_1fr] gap-3 md:gap-4">
+                <GameIcon game={match.game} />
+                <Team team={match.home} />
+                <div className="flex flex-col items-center">
+                  <span className="text-sm text-center md:text-sm">
+                    {dayjs(match.date).tz("America/Sao_Paulo").format("DD/MMM")}
+                  </span>
+                  <span className="text-4xl">
+                    {dayjs(match.date).tz("America/Sao_Paulo").format("HH:mm")}
+                  </span>
+                  <span className="text-[8px] text-center md:text-xs">
+                    {match.league}
+                  </span>
+                </div>
+                <Team team={match.away} />
+                <div className="flex items-center mr-1">
+                  <AddToCalendar match={match} />
+                </div>
               </div>
-              <Team team={match.away} />
-              <div className="flex items-center mr-1">
-                <AddToCalendar match={match} />
-              </div>
+              {matches[index + 1] &&
+              dayjs(matches[index + 1]?.date).date() >
+                dayjs(match.date).date() ? (
+                <span className="block w-full h-5" />
+              ) : (
+                <></>
+              )}
             </div>
           ))}
         </div>
