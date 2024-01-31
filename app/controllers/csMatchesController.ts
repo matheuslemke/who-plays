f3bba9c1f97531351db540c3b4c075124b96a4e8
Anderson Lemke <amatheuslemke@hotmail.com>
2024-01-31 10:57:50 -0300

    fix: furia team id

--- app/controllers/csMatchesController.ts
+++ app/controllers/csMatchesController.ts
@@ -2,7 +2,7 @@ import { CSMatchesResource } from "@/resources/csMatchesResource"
 import { Match } from "@/types/Match"
 
 const getMatches = async (): Promise<Match[]> => {
-  const furiaId = 364396
+  const furiaId = 364252
   const mibrId = 366538
   const imperialId = 365850
   const planoId = 381450
